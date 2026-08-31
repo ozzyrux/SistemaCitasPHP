509ed23bb914b5ec1452b949ae44dc0d2724efdd
ozzyrux <bryanbenitez060@gmail.com>
2026-08-31 13:44:49 -0600

    Corrigiendo sintaxis y dependencias de Vue

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            vue(),
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-        }),
-        tailwindcss(),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
+        vue(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
